96ee0b7ed4261d1ef23b3fcec5f8968c13830c01
jdbriner07 <jdbriner07@gmail.com>
2017-04-10 11:33:27 -0700

    fixed movement so that is is no longer stuttering

--- client/js/game.js
+++ client/js/game.js
@@ -14,7 +14,7 @@ Game.create = function() {
 	//Game.add.sprite(0, 0, 'background');
 	Client.askNewPlayer();
 	Game.cursor = {x: 450, y: 300};
-	Game.pulse = setInterval(Game.heartBeat, 300);
+	Game.pulse = setInterval(Game.heartBeat, 16);
 }
 
 Game.update = function() {
@@ -28,7 +28,8 @@ Game.heartBeat = function() {
 Game.addNewPlayer = function(id, x, y) {
 	Game.playerMap[id] = Game.add.sprite(x, y, 'character');
 	Game.playerMap[id].anchor.x = 0.5;
-	Game.playerMap[id].anchor.y = 0.5;	
+	Game.playerMap[id].anchor.y = 0.5;
+	game.physics.enable(Game.playerMap[id]);	
 }
 
 Game.remove = function(id) {
@@ -39,13 +40,25 @@ Game.remove = function(id) {
 Game.updatePlayer = function(id, x, y) {
 	var player = Game.playerMap[id];
 	if (player) {
-		//try making this velocity to git rid of the jerkyness
+		var velocity = 1000;
+		var xDistance = x - player.x;
+		var yDistance = y - player.y;
+		var distance = Game.distance(player.x, player.y, x, y);
+		var yVelocity = yDistance/distance;
+		var xVelocity = xDistance/distance;
+		if (Math.abs(yDistance) < 5 && Math.abs(xDistance) < 5) {
+			player.body.velocity.x = 0;
+			player.body.velocity.y = 0;
+		} else {
+		player.body.velocity.y = yVelocity * velocity;
+		player.body.velocity.x = xVelocity * velocity;
+		}
 
-		var tween = Game.add.tween(player);
-		var travel = Game.distance(player.x, player.y, x, y);
-		var time = travel * 3;
-		tween.to({x: x, y: y}, time);
-		tween.start();
+		// var tween = Game.add.tween(player);
+		// var travel = Game.distance(player.x, player.y, x, y);
+		// var time = travel * 3;
+		// tween.to({x: x, y: y}, time);
+		// tween.start();
 	}
 }
 
