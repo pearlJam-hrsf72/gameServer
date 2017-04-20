37bd7d84318cdd30640f30843dbf0b508162c1e8
jdbriner07 <jdbriner07@gmail.com>
2017-04-19 17:01:36 -0700

    fixed problem where hoes would keep adding more in

--- server/socket.js
+++ server/socket.js
@@ -115,7 +115,7 @@ module.exports = function(io) {
       clearInterval(heartbeat);
       updatePlayerStatsInDatabase();
       dbPlayers = [];
-
+      interactions.holeCenters = [];
     } // end gameOver
 
 
