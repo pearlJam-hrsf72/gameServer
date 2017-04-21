6893eaf039d4915a1afd541edfa821a770906e2b
jdbriner07 <jdbriner07@gmail.com>
2017-04-21 11:05:01 -0700

    got past cors, figuring out how to send a file

--- gameServer.js
+++ gameServer.js
@@ -13,13 +13,6 @@ var dataBase = require('./server/dataBase.js')
 
 var socketManager = require('./server/socket.js')(io);
 
-const defaultCorsHeaders = {
-  origin: '*',
-  methods: 'GET, POST, PUT, OPTIONS',
-  headers: 'content-type, accept',
-  age: 10 // Seconds.
-};
-
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
@@ -30,39 +23,13 @@ app.options('*', function(req, res, next){
     res.end();
 });
 
-// app.use(cors({
-// 	allowedOrigins: ['http://localhost:3000/', 'http://localhost:8080/']
-// }))
-
 app.use('/sockets', express.static(__dirname + '/client/sockets'));
 app.use('/js', express.static(__dirname + '/client/js'));
 app.use('/assets', express.static(path.join(__dirname + '/client/assets')));
 app.use('/css', express.static(path.join(__dirname + '/client/css')));
 
-// app.use(function(req, res, next) {
-// 	if (req.method === 'OPTIONS') {
-// 		// console.log(req.headers);
-// 		//need to write the header to be the defaultcorsheader so that the options request will accept the preflight response
-// 		// res.header('access-control-allow-origin', defaultCorsHeaders.origin);
-// 		// res.header('access-control-allow-methods', defaultCorsHeaders.methods);
-// 		// res.header('access-control-allow-header', defaultCorsHeaders.headers);
-// 		// res.header('access-control-max-age', defaultCorsHeaders.age);
-// 		res.setHeader("Access-Control-Allow-Origin", "*");
-//     	res.setHeader("Access-Control-Allow-Credentials", "true");
-//     	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-// 		res.sendStatus(200);
-// 	} else {	
-// 		next();
-// 	}
-// })
-
 app.get('/', function(req, res) {
-  // res.sendFile(__dirname + '/client/index.html');
-  // res.header('Access-Control-Allow-Origin', '*');
-  console.log('recieved a get request');
-  res.send('this is from Jeff');
+  res.sendFile(__dirname + '/client');
 });
 
 app.get('/spectate', function(req, res) {
