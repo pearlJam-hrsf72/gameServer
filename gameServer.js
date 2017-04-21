1f41f22a19cf5a791f917e86f55c4364bd988530
jdbriner07 <jdbriner07@gmail.com>
2017-04-20 17:16:43 -0700

    still trying to get cors working

--- gameServer.js
+++ gameServer.js
@@ -35,6 +35,11 @@ app.get('/spectate', function(req, res) {
 	res.sendFile(__dirname + '/client/spectate.html');
 });
 
+app.use(function(req, res) {
+	console.log(req);
+	res.sendStatus(200);
+})
+
 server.listen(process.env.PORT || 3005, function() {
   console.log(`gameServer is listening on PORT ${process.env.port || 3005}`);
 });
