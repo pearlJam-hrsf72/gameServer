3d923c4502ecff3a389b7a06b17a1e0134d550e2
jdbriner07 <jdbriner07@gmail.com>
2017-04-20 17:05:48 -0700

    trying to get cors to work with main server

--- gameServer.js
+++ gameServer.js
@@ -26,7 +26,7 @@ app.use('/css', express.static(path.join(__dirname + '/client/css')));
 
 app.get('/', function(req, res) {
   // res.sendFile(__dirname + '/client/index.html');
-  res.headers = defaultCorsHeaders;
+  res.header(defaultCorsHeaders);
   res.send('hi from the heroku gameServer');
 });
 
