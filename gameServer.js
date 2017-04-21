2fa741dfeaf108feb0a015e1818dbdbebc86c910
jdbriner07 <jdbriner07@gmail.com>
2017-04-21 11:20:34 -0700

    trying to send files to other client

--- gameServer.js
+++ gameServer.js
@@ -15,7 +15,7 @@ var socketManager = require('./server/socket.js')(io);
 
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'json');
     next();
 })
 
@@ -37,7 +37,7 @@ app.get('/spectate', function(req, res) {
 });
 
 app.get('/files', function(req, res) {
-  res.sendFile(__dirname + '/client');
+  res.sendFile(__dirname + '/client/js');
 })
 
 
