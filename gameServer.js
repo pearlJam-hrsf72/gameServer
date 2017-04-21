7b1ab824c5a5015a74c304d4690b446fe7d0f054
jdbriner07 <jdbriner07@gmail.com>
2017-04-20 19:40:08 -0700

    still debugging cors

--- gameServer.js
+++ gameServer.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cors = require('express-cors');
 
 var app = express();
 var server = require('http').Server(app);
@@ -19,30 +20,33 @@ const defaultCorsHeaders = {
   age: 10 // Seconds.
 };
 
+app.use(cors({
+	allowedOrigins: ['http://localhost:3000/', 'http://localhost:8080/']
+}))
 
 app.use('/sockets', express.static(__dirname + '/client/sockets'));
 app.use('/js', express.static(__dirname + '/client/js'));
 app.use('/assets', express.static(path.join(__dirname + '/client/assets')));
 app.use('/css', express.static(path.join(__dirname + '/client/css')));
 
-app.use(function(req, res, next) {
-	if (req.method === 'OPTIONS') {
-		// console.log(req.headers);
-		//need to write the header to be the defaultcorsheader so that the options request will accept the preflight response
-		// res.header('access-control-allow-origin', defaultCorsHeaders.origin);
-		// res.header('access-control-allow-methods', defaultCorsHeaders.methods);
-		// res.header('access-control-allow-header', defaultCorsHeaders.headers);
-		// res.header('access-control-max-age', defaultCorsHeaders.age);
-		res.setHeader("Access-Control-Allow-Origin", "*");
-    	res.setHeader("Access-Control-Allow-Credentials", "true");
-    	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+// app.use(function(req, res, next) {
+// 	if (req.method === 'OPTIONS') {
+// 		// console.log(req.headers);
+// 		//need to write the header to be the defaultcorsheader so that the options request will accept the preflight response
+// 		// res.header('access-control-allow-origin', defaultCorsHeaders.origin);
+// 		// res.header('access-control-allow-methods', defaultCorsHeaders.methods);
+// 		// res.header('access-control-allow-header', defaultCorsHeaders.headers);
+// 		// res.header('access-control-max-age', defaultCorsHeaders.age);
+// 		res.setHeader("Access-Control-Allow-Origin", "*");
+//     	res.setHeader("Access-Control-Allow-Credentials", "true");
+//     	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-		res.sendStatus(200);
-	} else {	
-		next();
-	}
-})
+// 		res.sendStatus(200);
+// 	} else {	
+// 		next();
+// 	}
+// })
 
 app.get('/', function(req, res) {
   // res.sendFile(__dirname + '/client/index.html');
