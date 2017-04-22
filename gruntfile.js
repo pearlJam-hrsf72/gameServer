0e15eae68349f0644bcb1a8cc62cf87cb0626f8b
jdbriner07 <jdbriner07@gmail.com>
2017-04-22 12:09:37 -0700

    this is an auto-commit through grunt

--- gruntfile.js
+++ gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
         ].join(' && ')
       },
       heroku: {
-        command: 'gphm'
+        command: 'git push heroku +HEAD:master'
       }
     }
   });
